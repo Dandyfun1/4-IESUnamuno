085c1799bdc65266e5caa03db709ef13c26a1a03
Dandyfun <124087430+Dandyfun1@users.noreply.github.com>
2026-02-11 13:13:20 +0100

    Delete scrpt.js

--- scrpt.js
+++
@@ -1,7 +0,0 @@
-/*Start of my grade's own website.This website will be developed by all who want to be part of it and will be accessible to all who need it. The main idea of this website is to help organize all school activities in to a single place to make life easier for the users and make it a smooth run through 4º grade.*/
-Var1 = "room"
-Var2 = "main"
-Var3 = "Jonathan"
-Var4 = "settings"
-Var5 = "ADMIN"
-console.log(Var1 , Var2 , Var3 , Var4 , Var5);
